54c716d639a9626eaf7fd76ff2140b72e0c98b7e
Fitz <f18846188605@gmail.com>
2017-07-16 21:58:18 +0800

    1

--- system/models.py
+++ system/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    StudentID=models.CharField(primary_key=True,max_length=20)
+    Name=models.CharField(max_length=20)
+    Chinese=models.FloatField(max_length=5,default=0)
+    Math=models.FloatField(max_length=5,default=0)
+    English=models.FloatField(max_length=5,default=0)
+    Physics=models.FloatField(max_length=5,default=0)
+    Chemistry=models.FloatField(max_length=5,default=0)
+    password=models.CharField(max_length=20)
+    sum=models.FloatField(max_length=5,default=0)
+    def __str__(self):
+        return self.id,self.Name
+
+class Teacher(models.Model):
+    TeacherID=models.CharField(primary_key=True,max_length=20)
+    Name=models.CharField(max_length=20)
+    Password=models.CharField(max_length=20)
+    def __str__(self):
+        return self.id,self.Name
