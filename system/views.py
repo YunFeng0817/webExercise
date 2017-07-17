116d1fa9cbb65d44174e265f970ccf5d10410a4b
Fitz <f18846188605@gmail.com>
2017-07-17 21:58:43 +0800

    4

--- system/views.py
+++ system/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse,HttpResponseRedirect,Http404
 from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from system.models import *
 # Create your views here.
-studentID=0
-teacherID=0
+
 def login(request):
     return render(request,'system/login.html')
+
+
 def loginAction(request):
     if request.method=='POST':
         ID = request.POST.get('ID').strip()
@@ -16,8 +17,10 @@ def loginAction(request):
             if Student.objects.filter(StudentID=ID):
                 student=Student.objects.get(pk=ID)
                 if Password==student.password:
-                    studentID=ID
-                    return HttpResponseRedirect(reverse('system:login'))
+                    request.session['studentID']=ID
+                    lst=list(Student.objects.order_by('-sum'))
+                    Ranking=lst.index(Student.objects.get(pk=ID))+1
+                    return render(request,'system/student.html',{'student_score': student,'Ranking':Ranking})
                 else:
                     return render(request,'system/login.html',{'error':'你输入的密码错误'})
             else:
@@ -26,11 +29,20 @@ def loginAction(request):
             if Teacher.objects.filter(TeacherID=ID):
                 teacher=Teacher.objects.get(pk=ID)
                 if Password==teacher.password:
-                    teacherID=ID
-                    return HttpResponse('登陆成功')
+                    request.session['teacherID']=ID
+                    return HttpResponseRedirect(reverse('system/student.html'))
                 else:
                     return render(request, 'system/login.html', {'error': '你输入的密码错误'})
             else:
                 return HttpResponse('没有这个用户！')
     return render(request,'')
+
+
+
+def logoutAction(request):
+    try:
+        del request.session['studentID']
+    except KeyError:
+        pass
+    return HttpResponse('你已经成功退出')
 
